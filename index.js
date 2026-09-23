a17b9f4f893f1c7d0edd6f31eb6dfdb5a16a6b05
aldmf9813-rgb <aldmf9813@gmail.com>
2026-09-23 23:46:02 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import sub from './sub.js';
 
 /* =========== Client ========== */
 const client = new Client({
-  phoneNumber: '20123456789', // Bot number
+  phoneNumber: '2011 17873587', // Bot number
   prefix: [".", "/", "!"],
   fromMe: false, 
   owners: [
